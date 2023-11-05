9b4c78f8b7d68ae14a2feacfcbbcace1e3de6589
nyogimi <rhenzfernandez.rf@gmail.com>
2023-11-06 01:11:20 +0800

    update4

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import { HomePage } from "./Pages/HomePage";
 import { Services } from "./Pages/Services";
 import { AboutUs } from "./Pages/AboutUs";
 import { Contact } from "./Pages/Contact";
+import { LoginPage } from "./Pages/LoginPage";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/" element={<HomePage/>}/>
           <Route path="/services" element={<Services/>}/>
           <Route path="/aboutus" element={<AboutUs/>}/>
           <Route path="/contact" element={<Contact/>}/>
+          <Route path="/loginpage" element={<LoginPage/>}/>
         </Routes>
       </div>
-    </Router>
+    </Router> 
   );
 }
 

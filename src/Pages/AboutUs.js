9f0a810df7a29945c64916815221c1cfca33d9f6
Grant Geslani <qggcgeslani01@tip.edu.ph>
2023-11-06 00:05:33 +0800

    Update 4

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,142 +1,114 @@
 import React from "react";
 import "./style.css";
 
-export const Contact = () => {
+export const AboutUs = () => {
   return (
-    <div className="contact">
-      <div className="div">
+    <div className="about-us">
+      <div className="overlap-wrapper">
         <div className="overlap">
           <div className="overlap-group">
-            <div className="overlap-2">
-              <img className="ellipse" alt="Ellipse" src="image.svg" />
-              <p className="quezon-city-clinic">
-                Quezon City Clinic <br />
-                2nd Floor, #120 St&nbsp;&nbsp;Maginhawa St.,
-                <br />
-                Teachers Village. Quezon Cty <br />
-                <br />
-                Tue-Sun: 2:00 PM - 9:00&nbsp;&nbsp;PM
-                <br />
-                <br />
-                Mobile No: +63 9954471918
-              </p>
+            <img className="ellipse" alt="Ellipse" src="ellipse-2.svg" />
+            <img className="image" alt="Image" src="image-10.png" />
+            <div className="col-md">
+              <div className="headline">About Us</div>
             </div>
-            <div className="overlap-group-2">
-              <img className="img" alt="Ellipse" src="ellipse.svg" />
-              <div className="dark-theme">
-                <img
-                  className="element"
-                  alt="Element"
-                  src="21317736-166466953903791-3719905096119854242-n-removebg-preview-2.png"
-                />
-                <div className="collapse-navbar">
-                  <div className="navbar-nav">
-                    <div className="li">
-                      <div className="a">
-                        <div className="link">Home</div>
-                      </div>
-                    </div>
-                    <div className="a-wrapper">
-                      <div className="link-wrapper">
-                        <div className="text-wrapper">Services</div>
-                      </div>
-                    </div>
-                    <div className="div-wrapper">
-                      <div className="a-2">
-                        <div className="text-wrapper">About Us</div>
-                      </div>
-                    </div>
-                    <div className="li-2">
-                      <div className="a-3">
-                        <div className="link-2">Contact</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="nav-item-wrapper">
-                    <div className="nav-item">
-                      <div className="button-btn-primary">
-                        <div className="btn-text">Book Appointment</div>
-                        <img
-                          className="icn-arrow-right-icn"
-                          alt="Icn arrow right icn"
-                          src="icn-arrow-right-icn-xs.svg"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="headline">Dr. Lester R. Limbos</div>
-              <p className="h-headline">
-                Manila Clinic
-                <br />
-                Unit 102, BMC Bldg 3 #1146 Hermosa St., Abad Santos Ave., Manila
-                <br />
-                <br />
-                Tues/Wed/Thurs/Sat/Sun: 7:00 AM - 2:00 PM
-                <br />
-                <br />
-                Mobile No: +63 932 851 7356
-              </p>
-            </div>
-          </div>
-          <img className="image" alt="Image" src="image-15.png" />
-        </div>
-        <div className="overlap-3">
-          <img className="ellipse-2" alt="Ellipse" src="ellipse-2.svg" />
-          <p className="p">
-            St. Luke’s Medical Center
-            <br />
-            Room 1013 South Tower, CHBC E. Rodriguez Ave., Quezon City (By Appointment Only)
-            <br />
-            <br />
-            Telephone No: (02) 723 0101&nbsp;&nbsp;loc 2013
-            <br />
-            Mobile No: +63 917 828 0047
-          </p>
-          <img className="image-2" alt="Image" src="image-16.png" />
-          <div className="content">
-            <div className="columns">
-              <div className="column">
-                <div className="info">
-                  <div className="frame">
-                    <p className="text-wrapper-2">Limbos Dental Cosmetics and Implant Center</p>
-                    <div className="p-s-card-paragraph">
-                      <p className="we-are-commited-to">
-                        We are commited to give you the best smile you deserve! If you have been feeling less
-                        than&nbsp;&nbsp;happy with your smile and want to do something about it, look no further! LIMBOS
-                        DENTAL COSMETICS AND IMPLANT CENTER has the tools to give you your best smile!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="menus">
-                <div className="div-s-card-text-wrapper">
-                  <div className="div-s-card-text">
-                    <div className="frame-2">
-                      <div className="text-wrapper-3">Main Clinic</div>
-                      <div className="p-s-card-paragraph-2">
-                        <p className="text-wrapper-4">
-                          120 Maginhawa Street, Teachers Village, Diliman Quezon City Room 1013 South, Tower, Cathedral
-                          Heights Bldg. Complex, E. Rodriguez Ave. Unit 201 BMCI Building 3, 1146 Hermosa Street Abad
-                          Santos, Manila., Quezon City, Philippines
+            <img className="img" alt="Ellipse" src="image.svg" />
+            <p className="h-headline">
+              Dentist Lester  R. Limbos established the Limbos Dental Cosmetics and Implant Center Dental Clinic by
+              himself in 2008, with its first location in 120 Maginhawa Street, Teachers Village, Diliman Quezon City.
+              Currently, he is the head and the sole dentist present in the clinic. Dr. Limbos is assisted by three
+              people, one secretary and two dental assistants, in order to facilitate the whole clinic
+            </p>
+            <div className="content">
+              <div className="columns">
+                <div className="column">
+                  <div className="info">
+                    <div className="frame">
+                      <p className="text-wrapper">Limbos Dental Cosmetics and Implant Center</p>
+                      <div className="p-s-card-paragraph">
+                        <p className="we-are-commited-to">
+                          We are commited to give you the best smile you deserve! If you have been feeling less
+                          than&nbsp;&nbsp;happy with your smile and want to do something about it, look no further!
+                          LIMBOS DENTAL COSMETICS AND IMPLANT CENTER has the tools to give you your best smile!
                         </p>
                       </div>
                     </div>
-                    <div className="frame-3">
-                      <div className="text-wrapper-3">Our Hotline</div>
-                      <div className="p-s-card-paragraph-2">
-                        <div className="text-wrapper-5">0917 828 0047</div>
+                  </div>
+                </div>
+                <div className="menus">
+                  <div className="div-s-card-text-wrapper">
+                    <div className="div-s-card-text">
+                      <div className="div">
+                        <div className="text-wrapper-2">Main Clinic</div>
+                        <div className="div-wrapper">
+                          <p className="p">
+                            120 Maginhawa Street, Teachers Village, Diliman Quezon City Room 1013 South, Tower,
+                            Cathedral Heights Bldg. Complex, E. Rodriguez Ave. Unit 201 BMCI Building 3, 1146 Hermosa
+                            Street Abad Santos, Manila., Quezon City, Philippines
+                          </p>
+                        </div>
+                      </div>
+                      <div className="frame-2">
+                        <div className="text-wrapper-2">Our Hotline</div>
+                        <div className="div-wrapper">
+                          <div className="text-wrapper-3">0917 828 0047</div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+              <img className="line" alt="Line" src="line-90.svg" />
+              <div className="bottom">
+                <p className="element-all-rights">© 2023, All Rights Reserved</p>
+              </div>
             </div>
-            <img className="line" alt="Line" src="line-90.svg" />
-            <div className="bottom">
-              <p className="element-all-rights">© 2023, All Rights Reserved</p>
+          </div>
+          <div className="overlap-group-2">
+            <img className="ellipse-2" alt="Ellipse" src="ellipse.svg" />
+            <div className="dark-theme">
+              <img
+                className="element"
+                alt="Element"
+                src="21317736-166466953903791-3719905096119854242-n-removebg-preview-2.png"
+              />
+              <div className="collapse-navbar">
+                <div className="navbar-nav">
+                  <div className="li">
+                    <div className="a">
+                      <div className="link">Home</div>
+                    </div>
+                  </div>
+                  <div className="a-wrapper">
+                    <div className="link-wrapper">
+                      <div className="link-2">Services</div>
+                    </div>
+                  </div>
+                  <div className="li-2">
+                    <div className="a-2">
+                      <div className="link-3">About Us</div>
+                    </div>
+                  </div>
+                  <div className="li-3">
+                    <div className="a-3">
+                      <div className="link-2">Contact</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="nav-item-wrapper">
+                  <div className="nav-item">
+                    <div className="button-btn-primary">
+                      <div className="btn-text">Book Appointment</div>
+                      <img className="icn-arrow-right-icn" alt="Icn arrow right icn" src="icn-arrow-right-icn-xs.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img className="image-2" alt="Image" src="image-8.png" />
+            <img className="image-3" alt="Image" src="image-11.png" />
+            <div className="headline-wrapper">
+              <div className="headline">Our Clients</div>
             </div>
           </div>
         </div>
